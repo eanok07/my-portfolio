1ee854d289a7b07b4deaba60e65ef628267776b7
EANOK R <eanok072000@gmail.com>
2024-11-26 12:28:11 +0530

    Initial commit

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,12 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home">
-      <marquee>
+      
    
 
       <h1>Welcome to My Portfolio</h1>
       <p>Hi, I'm EANOK R, a passionate FULL STACK DEVELOPER !</p>
-      </marquee>
+      
     </div>
   );
 };
